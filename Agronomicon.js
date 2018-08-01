4f76555cacf11559c9fc1e08f02879d73d08f9e9
Fedor Uvarov <acharvak@fsfe.org>
2018-08-01 16:01:12 +0200

    Update to 2.0109 (no actual changes except version number; compatible as is)

--- Agronomicon.js
+++ Agronomicon.js
@@ -26,9 +26,9 @@ SOFTWARE.
 
 // ======= DATA =======
 
-var VERSION = 2.0106;
-var REVISION = 0;       // <<<RELEASE: var REVISION = $REVISION;>>>
-var IS_DEV = true;      // <<<RELEASE: var IS_DEV = false;>>>
+var VERSION = 2.0109;
+var REVISION = 1;       // <<<RELEASE: var REVISION = $REVISION;>>>
+var IS_DEV = false;     // <<<RELEASE: var IS_DEV = false;>>>
 var IS_BETA = true;     // <<<RELEASE: var IS_BETA = $IS_BETA;>>>
 
 var RECIPE_AUTOUNLOCKED = 1;
