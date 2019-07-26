a6a4b8d8f65f6947c696639385c06372fa70941a
Renaud Douze <renauddouze@gmail.com>
2019-07-26 14:56:12 +0200

    Update to 2.019 by just changing the version number

--- Agronomicon.js
+++ Agronomicon.js
@@ -26,9 +26,9 @@ SOFTWARE.
 
 // ======= DATA =======
 
-var VERSION = 2.016;
+var VERSION = 2.019;
 var REVISION = 1;       // <<<RELEASE: var REVISION = $REVISION;>>>
-var IS_DEV = true;      // <<<RELEASE: var IS_DEV = false;>>>
+var IS_DEV = false;     // <<<RELEASE: var IS_DEV = false;>>>
 var IS_BETA = false;    // <<<RELEASE: var IS_BETA = $IS_BETA;>>>
 
 var RECIPE_AUTOUNLOCKED = 1;
